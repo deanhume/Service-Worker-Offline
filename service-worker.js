13c8abed41174e8ade59a4ee9516a6819bec623d
Dean <deanhume@gmail.com>
2016-01-21 17:00:21 +0000

    Made the offline pages prettier :+1:

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,39 @@
 'use strict';
 
-var CACHE_VERSION = 1;
-var CURRENT_CACHES = {
-  offline: 'offline-cache' + CACHE_VERSION
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
 };
-const OFFLINE_URL = 'offline-page.html';
+const offlineUrl = 'offline-page.html';
 
-self.addEventListener('install', event => {
+this.addEventListener('install', event => {
   event.waitUntil(
-    // We can't use cache.add() here, since we want OFFLINE_URL to be the cache key, but
-    // the actual URL we end up requesting might include a cache-busting parameter.
-    fetch(OFFLINE_URL).then(function(response) {
-      return caches.open(CURRENT_CACHES.offline).then(function(cache) {
-        console.log('adding offline page to cache', error);
-        return cache.put(OFFLINE_URL, response);
-      });
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          '/img/offline.svg',
+          offlineUrl
+      ]);
     })
   );
 });
 
-
-
-self.addEventListener('fetch', event => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  // request.mode of 'navigate' is unfortunately not supported in Chrome
-  // versions older than 49, so we need to include a less precise fallback,
-  // which checks for a GET request with an Accept: text/html header.
-  if (event.request.mode === 'navigate' ||
-      (event.request.method === 'GET' &&
-       event.request.headers.get('accept').includes('text/html'))) {
-    console.log('Handling fetch event for', event.request.url);
-    event.respondWith(
-      fetch(event.request.url).catch(error => {
-        // The catch is only triggered if fetch() throws an exception, which will most likely
-        // happen due to the server being unreachable.
-        // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
-        // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
-        // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
-        console.log('Fetch failed; returning offline page instead.', error);
-        return caches.match(OFFLINE_URL);
-      })
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+          })
     );
   }
+  else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
